8e3fef81de17261d1cedb804b3b122821ec5c4bd
Aakash Agarwal <agarwal7409@gmail.com>
2025-09-18 00:12:15 +0530

    Update main .dart

--- main .dart
+++ main .dart
@@ -6,7 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:login_app/registration_page.dart';
 import 'LogInDetails.dart';
 
-// trial comment 2
+// trial comment 3
 
 void main() {
   runApp(LogInMaterial());
